e8acef167ed2c3f06e29fbad50f6f81e159764b3
Krzysztof Frączek <fraczekkrzysztof@o2.pl>
2019-12-27 13:24:43 +0100

    first commit

---
+++ src/main/java/com/fraczekkrzysztof/goonbike/GoonbikeApplication.java
@@ -0,0 +1,13 @@
+package com.fraczekkrzysztof.goonbike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoonbikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoonbikeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fraczekkrzysztof/goonbike/GoonbikeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fraczekkrzysztof.goonbike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoonbikeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
