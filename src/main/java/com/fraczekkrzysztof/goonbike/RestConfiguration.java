9d60616e6f389d5062ea390fbddcb122c95278d6
Krzysztof Frączek <fraczekkrzysztof@o2.pl>
2019-12-27 18:22:19 +0100

    Add configurations

---
+++ src/main/java/com/fraczekkrzysztof/goonbike/RestConfiguration.java
@@ -0,0 +1,26 @@
+package com.fraczekkrzysztof.goonbike;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.Type;
+
+@Configuration
+public class RestConfiguration implements RepositoryRestConfigurer {
+
+    @Autowired
+    private EntityManager entityManager;
+
+    //this one is responsible for expose ids
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        config.exposeIdsFor(
+                entityManager.getMetamodel().getEntities().stream()
+                        .map(Type::getJavaType)
+                        .toArray(Class[]::new));
+    }
+
+}
