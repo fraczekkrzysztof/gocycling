b14b650f05be94434c7f8de97ba0e9a1959b23ff
Krzysztof Frączek <fraczekkrzysztof@o2.pl>
2020-01-29 19:26:26 +0100

    Refactor

--- src/main/java/com/fraczekkrzysztof/gocycling/rest/ConfirmationController.java
+++ src/main/java/com/fraczekkrzysztof/gocycling/rest/ConfirmationController.java
@@ -6,11 +6,13 @@ import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.resource.HttpResource;
 
 @RestController()
+@RequestMapping("api/custom/confirmations")
 public class ConfirmationController {
 
     ConfirmationService confirmationService;
@@ -20,7 +22,7 @@ public class ConfirmationController {
         this.confirmationService = confirmationService;
     }
 
-    @DeleteMapping("/api/custom/confirmations")
+    @DeleteMapping("/deleteByUserUidAndEventId")
     public ResponseEntity<String> deleteByUSerUidAndEventId(@RequestParam("userUid") String userUid, @RequestParam("eventId") long eventId){
         confirmationService.deleteByUserUidAndEventId(userUid,eventId);
         return ResponseEntity.status(HttpStatus.OK).body("");
