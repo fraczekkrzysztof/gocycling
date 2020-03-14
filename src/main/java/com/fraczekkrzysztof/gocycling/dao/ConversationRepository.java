89475c5cad2d56269a2decd8e3adbbc7607c3838
Krzysztof Frączek <fraczekkrzysztof@o2.pl>
2020-03-14 16:51:55 +0100

    Add Conversation entity

---
+++ src/main/java/com/fraczekkrzysztof/gocycling/dao/ConversationRepository.java
@@ -0,0 +1,20 @@
+package com.fraczekkrzysztof.gocycling.dao;
+
+import com.fraczekkrzysztof.gocycling.entity.Conversation;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import javax.transaction.Transactional;
+
+@RepositoryRestResource(path = "conversations")
+@Transactional
+public interface ConversationRepository extends JpaRepository<Conversation,Long>, ConversationRepositorySearch {
+
+    @Override
+    @Query("select c from Conversation c where c.event.id = :eventId")
+    Page<Conversation> findByEventId(@Param("eventId") long eventId, Pageable pageable);
+}
