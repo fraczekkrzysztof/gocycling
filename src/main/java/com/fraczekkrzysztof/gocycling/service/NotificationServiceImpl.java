b6554795ce7c798e64d2610c3ce0bec076290210
Krzysztof Frączek <fraczekkrzysztof@o2.pl>
2020-03-26 11:27:11 +0100

    Add text to NoSuchElementException

--- src/main/java/com/fraczekkrzysztof/gocycling/service/NotificationServiceImpl.java
+++ src/main/java/com/fraczekkrzysztof/gocycling/service/NotificationServiceImpl.java
@@ -2,14 +2,10 @@ package com.fraczekkrzysztof.gocycling.service;
 
 import com.fraczekkrzysztof.gocycling.dao.NotificationRepository;
 import com.fraczekkrzysztof.gocycling.entity.Notification;
-import com.fraczekkrzysztof.gocycling.service.NotificationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.Comparator;
 import java.util.List;
 import java.util.NoSuchElementException;
 
@@ -33,6 +29,6 @@ public class NotificationServiceImpl implements NotificationService {
     @Override
     public long getMaxNotificationIdForUser(String userUid) {
         List<Notification> notificationList = notificationRepository.findByUserUid(userUid, Pageable.unpaged()).getContent();
-        return notificationList.stream().mapToLong(Notification::getId).max().orElseThrow(NoSuchElementException::new);
+        return notificationList.stream().mapToLong(Notification::getId).max().orElseThrow(() -> new NoSuchElementException("There is no notification fo User"));
     }
 }
