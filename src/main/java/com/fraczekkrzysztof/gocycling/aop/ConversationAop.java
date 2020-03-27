b93b9ac4cb91bba366a42362191977e2eaab47f7
Krzysztof Frączek <fraczekkrzysztof@o2.pl>
2020-03-27 08:50:34 +0100

    Add notification for new Message

---
+++ src/main/java/com/fraczekkrzysztof/gocycling/aop/ConversationAop.java
@@ -0,0 +1,38 @@
+package com.fraczekkrzysztof.gocycling.aop;
+
+import com.fraczekkrzysztof.gocycling.entity.Conversation;
+import com.fraczekkrzysztof.gocycling.service.notification.NewConversationNotificationGenerator;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class ConversationAop {
+
+    NewConversationNotificationGenerator newConversationNotificationGenerator;
+
+    @Autowired
+    public ConversationAop(@Qualifier("newConversationNotificationGenerator") NewConversationNotificationGenerator newConversationNotificationGenerator ){
+        this.newConversationNotificationGenerator = newConversationNotificationGenerator;
+    }
+
+    @Pointcut("execution (* com.fraczekkrzysztof.gocycling.dao.ConversationRepository.save(..))")
+    private void forEventUpdate(){
+        //define pointcut
+    }
+
+    @Before("forEventUpdate()")
+    private void beforeUpdate(JoinPoint joinPoint){
+        for (Object arg : joinPoint.getArgs()){
+            if (arg instanceof Conversation){
+                Long id = ((Conversation)arg).getId();
+                if (id == 0 ) newConversationNotificationGenerator.addEventId(((Conversation)arg).getEvent().getId());
+            }
+        }
+    }
+}
