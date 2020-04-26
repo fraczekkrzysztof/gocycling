f4e9bee8469ea8d48c974c4a0c01d76018d627f0
Krzysztof Frączek <fraczekkrzysztof@o2.pl>
2020-04-26 15:50:15 +0200

    Add service to get list of user external apps

--- src/main/java/com/fraczekkrzysztof/gocycling/dao/UserExternalAppsRepository.java
+++ src/main/java/com/fraczekkrzysztof/gocycling/dao/UserExternalAppsRepository.java
@@ -3,8 +3,10 @@ package com.fraczekkrzysztof.gocycling.dao;
 import com.fraczekkrzysztof.gocycling.entity.UserExternalApp;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -12,5 +14,9 @@ public interface UserExternalAppsRepository extends JpaRepository<UserExternalAp
 
     @Override
     @Query("select uea from UserExternalApp uea join uea.user u where u.id = :userUid and uea.appType='STRAVA'")
-    Optional<UserExternalApp> findStravaByUserUid(String userUid);
+    Optional<UserExternalApp> findStravaByUserUid(@Param("userUid") String userUid);
+
+    @Override
+    @Query("select uea from UserExternalApp uea where uea.user.id = :userUid")
+    List<UserExternalApp> findExternalAppByUserUid(@Param("userUid")String userUid);
 }
