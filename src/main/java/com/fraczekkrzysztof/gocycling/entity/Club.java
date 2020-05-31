1e54296c14553ba05a548aa0633954bdd1a05a3c
Krzysztof Frączek <fraczekkrzysztof@o2.pl>
2020-05-31 18:06:46 +0200

    Add @Data annotation - hibernate doesn't work without it;

--- src/main/java/com/fraczekkrzysztof/gocycling/entity/Club.java
+++ src/main/java/com/fraczekkrzysztof/gocycling/entity/Club.java
@@ -2,6 +2,7 @@ package com.fraczekkrzysztof.gocycling.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
@@ -11,6 +12,7 @@ import javax.persistence.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Data
 public class Club {
 
     @Id
