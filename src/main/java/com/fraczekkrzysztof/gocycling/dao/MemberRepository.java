b91552862e93feb2ab9fb6847033694d3b32c223
Krzysztof Frączek <fraczekkrzysztof@o2.pl>
2020-06-10 18:12:32 +0200

    Add endpoint to find user membership information

--- src/main/java/com/fraczekkrzysztof/gocycling/dao/MemberRepository.java
+++ src/main/java/com/fraczekkrzysztof/gocycling/dao/MemberRepository.java
@@ -1,7 +1,10 @@
 package com.fraczekkrzysztof.gocycling.dao;
 
 import com.fraczekkrzysztof.gocycling.entity.Member;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import javax.transaction.Transactional;
@@ -9,4 +12,7 @@ import javax.transaction.Transactional;
 @RepositoryRestResource(path = "members")
 @Transactional
 public interface MemberRepository extends JpaRepository<Member,Long> {
+
+    @Query("select m from Member m join m.club c where c.id = :clubId and m.userUid = :userUid")
+    Page<Member> findAllByUserUidAndClubId(String userUid, long clubId, Pageable pageable);
 }
