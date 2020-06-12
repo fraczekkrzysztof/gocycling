92d92c1df95b3d8101968905932d44abf38e3155
Krzysztof Frączek <fraczekkrzysztof@o2.pl>
2020-06-12 20:27:50 +0200

    Add endpoint to find all user clubs

--- src/main/java/com/fraczekkrzysztof/gocycling/dao/ClubRepository.java
+++ src/main/java/com/fraczekkrzysztof/gocycling/dao/ClubRepository.java
@@ -1,7 +1,10 @@
 package com.fraczekkrzysztof.gocycling.dao;
 
 import com.fraczekkrzysztof.gocycling.entity.Club;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 
@@ -10,4 +13,7 @@ import javax.transaction.Transactional;
 @RepositoryRestResource(path = "clubs")
 @Transactional
 public interface ClubRepository extends JpaRepository<Club,Long> {
+
+    @Query("select c from Club c where c.userUid = :userUid")
+    Page<Club> findAllByUserUid(@Param("userUid") String userUid);
 }
