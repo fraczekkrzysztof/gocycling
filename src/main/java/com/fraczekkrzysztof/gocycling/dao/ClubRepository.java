bbbbdbef2e28736eccd7e4f61d56ec6cf712b043
Krzysztof Frączek <fraczekkrzysztof@o2.pl>
2020-06-16 18:54:32 +0200

    Add endpoint to retrieve single club for event id

--- src/main/java/com/fraczekkrzysztof/gocycling/dao/ClubRepository.java
+++ src/main/java/com/fraczekkrzysztof/gocycling/dao/ClubRepository.java
@@ -8,7 +8,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-
 import javax.transaction.Transactional;
 
 @RepositoryRestResource(path = "clubs")
@@ -17,4 +16,7 @@ public interface ClubRepository extends JpaRepository<Club,Long> {
 
     @Query("select c from Member m join m.club c where m.userUid = :userUid")
     Page<Club> findAllWhichUserIsMember(@Param("userUid") String userUid, Pageable page);
+
+    @Query("select c from Event e join e.club c where e.id = :eventId")
+    Page<Club> findClubForEventId(@Param("eventId") long eventId, Pageable page);
 }
