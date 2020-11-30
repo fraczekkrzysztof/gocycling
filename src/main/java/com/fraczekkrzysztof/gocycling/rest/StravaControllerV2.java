a1bd2243da58bc79b89f1bac796bf21f4b66d779
Krzysztof Frączek <fraczekkrzysztof@o2.pl>
2020-11-30 21:09:23 +0100

    Class rename

--- src/main/java/com/fraczekkrzysztof/gocycling/rest/StravaControllerV2.java
+++ src/main/java/com/fraczekkrzysztof/gocycling/rest/StravaControllerV2.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/v2/strava")
 @RequiredArgsConstructor
-public class StravaController {
+public class StravaControllerV2 {
 
     private final ExternalOAuthAuthorizer authorizer;
 
