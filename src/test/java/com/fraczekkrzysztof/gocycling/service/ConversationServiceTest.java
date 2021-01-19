2675f2095565c519d237e75a0ba2edb15c5beb98
Krzysztof Frączek <fraczekkrzysztof@o2.pl>
2021-01-19 18:28:24 +0100

    Add conversation service tests

---
+++ src/test/java/com/fraczekkrzysztof/gocycling/service/ConversationServiceTest.java
@@ -0,0 +1,111 @@
+package com.fraczekkrzysztof.gocycling.service;
+
+import com.fraczekkrzysztof.gocycling.dao.ConversationRepository;
+import com.fraczekkrzysztof.gocycling.dao.EventRepository;
+import com.fraczekkrzysztof.gocycling.dao.UserRepository;
+import com.fraczekkrzysztof.gocycling.dto.event.ConversationDto;
+import com.fraczekkrzysztof.gocycling.entity.Conversation;
+import com.fraczekkrzysztof.gocycling.entity.Event;
+import com.fraczekkrzysztof.gocycling.entity.User;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.within;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+@SpringBootTest
+public class ConversationServiceTest {
+
+    @MockBean
+    ConversationRepository conversationRepository;
+    @MockBean
+    UserRepository userRepository;
+    @MockBean
+    EventRepository eventRepository;
+
+    @Autowired
+    ConversationServiceV2 conversationService;
+
+    @Test
+    void shouldReturnConversationDtoList() {
+        User user1 = User.builder().name("Jarosław").id("123").build();
+        User user2 = User.builder().name("Mieczysłąw").id("456").build();
+
+        Conversation conversation1 = Conversation.builder()
+                .created(LocalDateTime.of(2021, 01, 19, 12, 00, 00))
+                .id(1L)
+                .message("Może podkręcimy trochę tempo?")
+                .user(user1).build();
+
+        Conversation conversation2 = Conversation.builder()
+                .created(LocalDateTime.of(2021, 01, 19, 12, 01, 00))
+                .id(2L)
+                .message("Panie mamy styczeń! Tlen trzeba robić!")
+                .user(user2).build();
+
+        when(conversationRepository.findByEventId(35L)).thenReturn(Arrays.asList(conversation1, conversation2));
+
+        ConversationDto expectedConversation1 = ConversationDto.builder()
+                .id(1L)
+                .created(LocalDateTime.of(2021, 01, 19, 12, 00, 00))
+                .userId("123")
+                .userName("Jarosław")
+                .message("Może podkręcimy trochę tempo?")
+                .build();
+        ConversationDto expectedConversation2 = ConversationDto.builder()
+                .id(2L)
+                .created(LocalDateTime.of(2021, 01, 19, 12, 01, 00))
+                .userId("456")
+                .userName("Mieczysłąw")
+                .message("Panie mamy styczeń! Tlen trzeba robić!")
+                .build();
+
+        //when
+        List<ConversationDto> receivedConversationList = conversationService.getAllByEventId(35L);
+
+        //then
+        assertThat(receivedConversationList).containsExactlyInAnyOrder(expectedConversation1, expectedConversation2);
+    }
+
+    @Test
+    void shouldAddConversation() {
+        Event mockEvent = mock(Event.class);
+        when(eventRepository.findById(4444L)).thenReturn(Optional.of(mockEvent));
+        User userWhichWantToAddConversation = mock(User.class);
+        when(userWhichWantToAddConversation.getId()).thenReturn("1234");
+        when(userWhichWantToAddConversation.getName()).thenReturn("Mieczysław");
+        when(userRepository.findById("1234")).thenReturn(Optional.of(userWhichWantToAddConversation));
+        ConversationDto givenConversationDto = ConversationDto.builder()
+                .userId("1234")
+                .message("Dodaje sobie wiadomość")
+                .build();
+
+        ConversationDto expectedConversationDto = ConversationDto.builder()
+                .message("Dodaje sobie wiadomość")
+                .userId("1234")
+                .userName("Mieczysław")
+                .build();
+
+        //when
+        ConversationDto returnedConversationDto = conversationService.addConversation(4444L, givenConversationDto);
+
+        //then
+        assertThat(returnedConversationDto).usingRecursiveComparison().ignoringFields("id", "created").isEqualTo(expectedConversationDto);
+        assertThat(returnedConversationDto.getCreated()).isCloseTo(LocalDateTime.now(), within(1, ChronoUnit.MINUTES));
+        assertThat(returnedConversationDto.getId()).isNotNull();
+
+    }
+}
