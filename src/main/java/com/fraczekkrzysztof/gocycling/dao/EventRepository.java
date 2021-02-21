655aab1638722523cb6d2ae9cb438e98e46c9649
Krzysztof Frączek <fraczekkrzysztof@o2.pl>
2021-02-21 12:21:05 +0100

    Events created by user should contains cancel events

--- src/main/java/com/fraczekkrzysztof/gocycling/dao/EventRepository.java
+++ src/main/java/com/fraczekkrzysztof/gocycling/dao/EventRepository.java
@@ -15,7 +15,7 @@ public interface EventRepository extends JpaRepository<Event, Long> {
     @Query("select e from Event e join e.club c where c.id = :id and e.dateAndTime >= :dateTime and e.canceled = false")
     Page<Event> findInFutureByClubId(@Param("id") long clubId, @Param("dateTime") LocalDateTime dateTime, Pageable pageable);
 
-    @Query("select e from Event e join e.user u join e.club c where c.id = :id and u.id = :userUid and e.dateAndTime >= :dateTime and e.canceled = false")
+    @Query("select e from Event e join e.user u join e.club c where c.id = :id and u.id = :userUid and e.dateAndTime >= :dateTime")
     Page<Event> findByClubIdAndOwner(@Param("id") long clubId, @Param("userUid") String userUid, @Param("dateTime") LocalDateTime dateTime, Pageable pageable);
 
     @Query("select e from Event e join e.club c join e.confirmationList cl join cl.user u where c.id = :id and u.id = :userUid and e.dateAndTime >= :dateTime and e.canceled = false")
